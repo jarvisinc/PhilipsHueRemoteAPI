7e674931e902c909fc0aa627e1af3daee8a850f0
Paul Shi <hipaulshi@gmail.com>
2013-11-07 12:56:01 -0800

    Transparent POST API Call Support
    The system now start support 'almost' transparent calls to this API as
    if it was for the native API.
    
    Example:
    
    Set light state [Official API
    Doc](http://developers.meethue.com/1_lightsapi.html#16_set_light_state).
    
    Official API Endpoint : /api/<username>/lights/<id>/state
    Official API Method: Put
    Official API Data: {"ct":153, "colormode":"ct"}
    
    Custom API Endpoint : localhost:5000/api/lights/<id>/state
    Custom API Method: POST
    Custom API Data: EXACT THE SAME AS ABOVE
    
    It should in theory support all official PUT API calls to the URL
    endpoint of /api/username/*******
    
    So any official /api/username/**********
    becomes localhost:5000/api/***********
    
    Have fun!

--- callapi.py
+++ callapi.py
@@ -1,3 +1,4 @@
+from flask import Flask,request, make_response, jsonify
 import requests;
 import json;
 from credentials import token,bridgeID;
@@ -10,6 +11,17 @@ on_message = 'clipmessage={ bridgeId: "'+bridgeID+'", clipCommand: { url: "/api/
 off_message = 'clipmessage={ bridgeId: "'+bridgeID+'", clipCommand: { url: "/api/0/groups/0/action", method: "PUT", body: {"on":false} } }'
 headers = {'content-type':ContentType}
 
+def constructCustomMsg(apiEndPoint, command):
+  #apiEndPoint and command needs to be string
+  #command needs to conform to json format
+  custom_control_message_structure = 'clipmessage={ bridgeId: "'+bridgeID+'", clipCommand: { url: "/api/0/'+apiEndPoint+'", method: "PUT", body: '+command+' } }'
+  return custom_control_message_structure
+
+def philipsControlCustom(msgToSent):
+  payload = {'token':token};
+  r = requests.post(API_ADDRESS_CONTROL, params=payload,headers=headers,data=msgToSent);
+  return r.text
+
 def philipsControl(action):
   payload = {'token':token};
   r = requests.post(API_ADDRESS_CONTROL, params=payload,headers=headers,data=messageToSent(action));
@@ -18,7 +30,7 @@ def philipsControl(action):
 def philipsInfo():
   payload = {'token': token, 'bridgeid': bridgeID};
   r = requests.get(API_STATUS_ADDRESS, params=payload)
-  # print (r.content.lights);
+  print (json.dumps(json.loads(r.content),indent=4))
   res = json.loads(r.content);
   # print (res['lights']['1']['name']);
   numreachable = 0;
@@ -48,3 +60,30 @@ def messageToSent(action):
   else:
     return "not correct instruction"
 
+def api(request,path):
+  print (request.headers)
+  print request.headers["Content-Type"]
+  print "Request Path: {}".format(path)
+  apiEndPoint=path
+  # Verify the JSON format
+  result = request.get_json(force=True, silent=True, cache=True)
+  if result:
+    print "JSON: {}".format(result)
+    print "JSON String: {}".format(json.dumps(result))
+    command = json.dumps(result)
+    apiEndPoint = path
+    contentToBeRequested=constructCustomMsg(apiEndPoint, command)
+    print "constructed custom msg: {}".format(contentToBeRequested)
+    print "sending control"
+    philipsControlCustom(contentToBeRequested)
+    response = make_response('{"status":200}')
+    response.mimetype='application/json'
+    return response
+  else:
+    error_msg = "not a valid json request"
+    response_json = jsonify(error=error_msg  ,
+                   request_received=request.data)
+    response = make_response(response_json)
+    response.mimetype='application/json'
+    print "Not valid JSON"
+    return response
--- hueapiserver.py
+++ hueapiserver.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 from callapi import *
 app = Flask(__name__)
 
@@ -14,7 +14,11 @@ def on():
 def off():
     return philipsControl("off");
 
+@app.route('/api/<path:path>',methods=['POST', 'GET'])
+def apiHandler(path):
+	return api(request,path);
 
 if __name__ == "__main__":
-      app.run(host='0.0.0.0')
+      app.run(host='0.0.0.0', debug=True)
+
 
