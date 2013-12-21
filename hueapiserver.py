0e1b8f4b813b811c4edc756d7cae4e274194eb6c
Jianer Shi (Paul) <hipaulshi@gmail.com>
2013-12-21 00:39:39 -0800

    test shows threaded server is far more stable

--- hueapiserver.py
+++ hueapiserver.py
@@ -20,6 +20,6 @@ def apiHandler(path):
 	return api(request,path);
 
 if __name__ == "__main__":
-      app.run(host='0.0.0.0', debug=True)
+      app.run(host='0.0.0.0', debug=True, threaded=True)
 
 
